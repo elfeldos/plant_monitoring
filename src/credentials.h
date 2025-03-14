6b755131b7d1e1fdf55c2025c28fff1e16db9088
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2025-03-14 15:36:02 +0100

    added wifi connection, mqtt cloud connection and reading received data

---
+++ src/credentials.h
@@ -0,0 +1,15 @@
+// credentials.h
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi credentials
+const char* ssid = "FRITZ!Box 6660 Cable VU";
+const char* password = "34952112804304908019";
+
+// HiveMQ Cloud MQTT Broker settings
+const char* mqtt_server = "b21378db9aee40c89e663438db56036a.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_username = "elfeldos";
+const char* mqtt_password = "elfeldosMQ_Hive1";
+
+#endif // CREDENTIALS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,78 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <credentials.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* mqtt_topic = "soil/moisture";
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+
+// SSL Certificate (cannot find it)
+const char* root_ca = \
+"-----BEGIN CERTIFICATE-----\n" \
+"... Your HiveMQ Cloud CA Certificate ...\n" \
+"-----END CERTIFICATE-----\n";
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("Connecting to Wifi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Wifi connected");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection to HiveMQ Cloud...");
+
+    // Create a client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+
+    // Attempt to connect with username and password
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" trying again in 5 seconds");
+      delay(5000);
+    }
+  }
+} 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  setup_wifi();
+  espClient.setInsecure();
+  client.setServer(mqtt_server, mqtt_port);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  unsigned long now = millis();
+  if (now - lastMsg > 10000) {  // Send data every 10 seconds
+    lastMsg = now;
+    
+    // Read soil moisture
+    int soilMoisture = analogRead(A0);
+    
+    // Convert to string and publish
+    String msg = String(soilMoisture);
+    client.publish(mqtt_topic, msg.c_str());
+    
+    Serial.print("Published soil moisture to HiveMQ Cloud: ");
+    Serial.println(soilMoisture);
+  }
+
 }
